3c7061a6ebea7b19629cbf904fe1e2ea5b233d41
App Academy Student <student@appacademy.io>
2016-12-27 15:52:49 -0800

    refactor main.jsx

--- skeleton/main.jsx
+++ skeleton/main.jsx
@@ -1,7 +1,11 @@
 import Tabs from './tabs';
 import React from 'react';
 
-const tabs = [{title: 'one', content: 'I am the first'}, {title: 'two', content: 'Be creative Nate'}, {title: 'three', content: 'react is cool!' }];
+const tabs = [
+  {title: 'one', content: 'I am the first'},
+  {title: 'two', content: 'I am the second'},
+  {title: 'three', content: 'react is cool!' }
+];
 
 const Main = () => (
   <div className="main">
